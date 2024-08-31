fa99d0c5d0d06975d766ddd869adaff53bc6b5b7
AliciaLevita <202001384a@utea.edu.pe>
2024-08-30 22:54:00 -0500

    correcciones

--- project/server.js
+++ project/server.js
@@ -15,9 +15,7 @@ app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
-
 app.listen(3000, () => {
     console.log('Express server started at port : 3000');
 });
-
 app.use('/employee', employeeController);
